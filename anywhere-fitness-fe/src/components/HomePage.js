43771ee0fc13867ed259a21b6be686368f1c63ac
Tory Rachel Van Tassel <countrygirlr09@hotmail.com>
2021-04-21 21:02:34 -0600

    Routed HomePage on app.js

---
+++ anywhere-fitness-fe/src/components/HomePage.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useState } from 'react';
+import { Link } from 'react-router-dom'
+
+
+
+
+export default function HomePage () {
+
+
+
+    return (
+        <div>
+            <Link to='/'> Sign Up </Link>
+            <Link to='/'> Sign In </Link>
+        </div>
+        )
+
+}
+
+
