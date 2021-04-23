d634001733f384c81141c3ccbf7dda8d7bd091e3
Tory Rachel Van Tassel <countrygirlr09@hotmail.com>
2021-04-22 20:12:31 -0600

    Moved Route to Switch

--- anywhere-fitness-fe/src/App.js
+++ anywhere-fitness-fe/src/App.js
@@ -12,18 +12,17 @@ function App() {
         <nav>
           <h1>Anywhere Fitness</h1>
           <div>
-            <Route exact path = "/"/>
-              <HomePage />
-            <Route/>
             <Link to="/">Home</Link>
             <Link to="/classes" component={CourseList}>Class List</Link>
             <Link to="/create">Create</Link>
             {/* Create should only display for authenticated instructors */}
             <Link to="/profile">Profile</Link>
+            
           </div>
         </nav>
       </header>
       <Switch>
+        <Route exact path = "/"/><HomePage /><Route/>
         <ProtectedRoute exact path='/classes' component={CourseList} />
       </Switch>
     </div>
