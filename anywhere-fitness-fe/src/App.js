aaa1751e9eb52b9d0ad4bf296850a1a35719de18
Tory Rachel Van Tassel <countrygirlr09@hotmail.com>
2021-04-22 20:56:05 -0600

    Created Login/Register buttons

--- anywhere-fitness-fe/src/App.js
+++ anywhere-fitness-fe/src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
-import { Route, Switch, Link, useHistory } from "react-router-dom";
+import { Route, Switch, Link, useHistory, useLocation } from "react-router-dom";
 import HomePage from './components/HomePage';
 import CourseList from './components/CourseList';
 import Course from "./components/Course";
 import ProtectedRoute from './components/ProtectedRoute';
+import Login from './components/Login';
+import Register from './components/Register';
+
+
 
 function App() {
+
+  const loginPage = useLocation("/login")
+
+
   return (
     <div>      
       <header className="App-header">
@@ -17,13 +25,17 @@ function App() {
             <Link to="/create">Create</Link>
             {/* Create should only display for authenticated instructors */}
             <Link to="/profile">Profile</Link>
-            
+            <div>
+              {loginPage ? <Link to= "/register"><button> Register </button></Link> : <Link to= "/login"><button> Login </button></Link>}
+            </div>
           </div>
         </nav>
       </header>
       <Switch>
         <Route exact path = "/"/><HomePage /><Route/>
         <ProtectedRoute exact path='/classes' component={CourseList} />
+        <Route path= "/login" component={Login}/>
+        <Route path= "/register" component={Register}/>
       </Switch>
     </div>
   );
