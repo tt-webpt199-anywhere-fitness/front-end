1f2725552d2d2e4d0db10fde61fd29795dacfd27
Kishkuman13 <73566523+Kishkuman13@users.noreply.github.com>
2021-04-25 20:12:32 -0600

    started to add api calls for login and registering

--- anywhere-fitness-fe/src/components/Register.js
+++ anywhere-fitness-fe/src/components/Register.js
@@ -2,99 +2,82 @@ import React from 'react';
 import axios from 'axios';
 import { useState } from 'react';
 
+const initialValue = {
+  username: "",
+  password: "",
+  role_id: 2
+}
 
-export default function Register(){
-    const [initialValue, setInitialValue] = useState({
-        user_name: "",
-        email_address: "",
-        password: "",
-        fitness_interests: ""
+export default function Register() {
+  const [newUser, setNewUser] = useState(initialValue)
 
+  const onChange = e => {
+    // let value = e.target.type === "radio" ? e.target.checked : e.target.value
+    setNewUser({
+      ...newUser,
+      [e.target.name]: e.target.value
     })
-
-    const onChange = e => {
-        e.persist()
-        let value = e.target.type === "radio" ? e.target.checked : e.target.value
-        setInitialValue({
-            ...initialValue, 
-            [e.target.name] : value
-        })
-    }
+  }
 
 
-    const onSubmit = e => {
-        e.preventDefault()
-        axios.post(`https://reqres.in/api/users`, initialValue)
-        .then(res => {
-            console.log(res.data)
-        })
-        .catch(error => console.log(error))
-    }
+  const onSubmit = e => {
+    e.preventDefault()
+    console.log(newUser)
+    axios.post(`https://anywhere-fitness-wpt199-be.herokuapp.com/api/auth/register`, initialValue)
+      .then(res => {
+        console.log(res.data)
+      })
+      .catch(error => console.log(error))
+  }
 
 
-    return(
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
         <div>
-            <form onSubmit={onSubmit}>
-              <div>
-               <label for="user_name"> Enter Name </label>
-               <input 
-                 type="text"
-                 name="user_name"
-                 id="user_name"
-                 placeholder='enter your name'
-                 onChange={onChange}
-                 value={initialValue.user_name}
-               />
+          <label for="username"> Enter Name </label>
+          <input
+            type="text"
+            name="username"
+            id="username"
+            placeholder='enter your name'
+            onChange={onChange}
+            value={newUser.username}
+          />
 
-               
-    
-    
-               <label for="email_address"> Email Address </label>  
-               <input 
-                  type="text"
-                  name="email_address"
-                  id="email_address"
-                  placeholder='enter your email address'
-                  onChange={onChange}
-                  value={initialValue.email_address}
-                />
-    
-               <div>
-                  <label for="password"> Password </label>
-                    <input
-                     name="password" 
-                     id="password" 
-                     onChange={onChange} 
-                     value={initialValue.password}
-                    />
-              
-                </div>
 
-                <div>
-                  <label for="interests"> Fitness Interests </label>
-                    <select
-                     name="interests" 
-                     id="interests" 
-                     onChange={onChange} 
-                     value={initialValue.fitness_interests}
-                     >
-                      <option value="">---Select One---</option>
-                      <option value="yoga"> Yoga </option>
-                      <option value="weights"> Weights </option>
-                      <option value="cardio"> Cardio </option>
-                      <option value=""></option>
-                    </select>
-                  
-                </div>
-    
-            <div>
-                <button> Submit </button>
-            </div>
-            
-             
-              </div>
-            
-          </form>
+          <div>
+            <label for="password"> Password </label>
+            <input
+              name="password"
+              id="password"
+              onChange={onChange}
+              value={newUser.password}
+            />
+
+          </div>
+
+          <div>
+            <label for="interests"> Instructor </label>
+            <input
+              name="role_id"
+              id="instructor"
+              type='checkbox'
+              onChange={onChange}
+              value='1'
+            >
+            </input>
+
+          </div>
+
+          <div>
+            <button> Submit </button>
+          </div>
+
+
         </div>
-    )
+
+      </form>
+    </div>
+  )
 }
