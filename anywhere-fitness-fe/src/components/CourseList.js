f633b97611d7b7ed42c5cff577992849321a3652
PVigar88 <Pvigar@gmail.com>
2021-04-27 23:20:54 -0400

    search function attempt?

--- anywhere-fitness-fe/src/components/CourseList.js
+++ anywhere-fitness-fe/src/components/CourseList.js
@@ -1,36 +1,53 @@
-import React, { useEffect, useState } from 'react'
-import Course from './Course'
-import axiosWithAuth from '../utils/axiosWithAuth'
+import React, { useEffect, useState } from "react";
+import Course from "./Course";
+import axiosWithAuth from "../utils/axiosWithAuth";
 
 const CourseList = () => {
-  const [courses, setCourses] = useState([])
+  const [courses, setCourses] = useState([]);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
-    getCourses()
-  }, [])
-
-  const getCourses = () => {
-    const axios = axiosWithAuth()
-    axios.get(`https://anywhere-fitness-wpt199-be.herokuapp.com/api/courses`)
-      .then(res => {
-        console.log('getCourses results', res)
-        setCourses(res.data)
+    const axios = axiosWithAuth();
+    axios
+      .get(`https://anywhere-fitness-wpt199-be.herokuapp.com/api/courses`)
+      .then((res) => {
+        console.log(res);
+        setCourses(res.data);
+        const searchArray = courses.filter((course) => {
+          return course.course.toLowerCase().includes(search.toLowerCase());
+        });
+        console.log(searchArray);
+        console.log("getCourses results", res);
+        setCourses(searchArray);
       })
-      .catch(err => console.log(err))
-  }
+      .catch((err) => console.log(err));
+  }, [search]);
+
+  const searchChange = (e) => {
+    setSearch(e.target.value);
+  };
 
   return (
     <div>
-      <div className='courses'>
-        {
-          courses &&
+      <form>
+        <label>
+          Search: &nbsp;
+          <input
+            type="text"
+            name="searchbar"
+            onChange={searchChange}
+            value={search}
+          />
+        </label>
+      </form>
+      <div className="courses">
+        {courses &&
           courses.map((course, index) => {
-            return <Course course={course} key={index} />
-          })
-        }
+            return <Course course={course} key={index} />;
+          })}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default CourseList
+export default CourseList;
